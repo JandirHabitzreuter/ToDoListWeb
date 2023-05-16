8b408fbdaba63cbce147eb3d3efd8d3732cf9cee
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2023-05-16 20:52:23 -0300

    fix update

--- BackEnd/src/modules/list/useCases/updateList/UpdateListController.ts
+++ BackEnd/src/modules/list/useCases/updateList/UpdateListController.ts
@@ -5,9 +5,10 @@ export class UpdateListController{
 
     async handle(request : Request, response : Response){
       const { content, Iscompleted } = request.body;
+      const { id } = request.params;
 
       const updateListUseCase = new UpdateListUseCase();
-      const list = updateListUseCase.execute(content, Iscompleted);
+      const list = updateListUseCase.execute(id, content, Iscompleted);
       return response.json(list);
     }
 } 
--- BackEnd/src/modules/list/useCases/updateList/UpdateListUseCase.ts
+++ BackEnd/src/modules/list/useCases/updateList/UpdateListUseCase.ts
@@ -3,10 +3,14 @@ import { prisma } from "../../../../database/prismaClient";
 
 export class UpdateListUseCase{
 
-    async execute(content : string, Iscompleted : boolean){
-        const list = await prisma.todolist.create({
+    async execute(id : string, content : string, isCompleted : boolean){
+        const list = await prisma.todolist.update({
+            where:{
+                id
+            },
             data:{
-                content
+                content,
+                isCompleted
             }
         });
 
