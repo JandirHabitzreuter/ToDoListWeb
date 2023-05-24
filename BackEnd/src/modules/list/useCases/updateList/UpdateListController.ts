b0097f0a9516f74e7c421c3dbe093c0b0cc01084
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2023-05-23 21:27:43 -0300

    fix isCompleted todo list

--- BackEnd/src/modules/list/useCases/updateList/UpdateListController.ts
+++ BackEnd/src/modules/list/useCases/updateList/UpdateListController.ts
@@ -3,12 +3,11 @@ import { UpdateListUseCase } from "./UpdateListUseCase";
 
 export class UpdateListController{
 
-    async handle(request : Request, response : Response){
-      const { content, Iscompleted } = request.body;
+    async handle(request : Request, response : Response){     
       const { id } = request.params;
 
       const updateListUseCase = new UpdateListUseCase();
-      const list = updateListUseCase.execute(id, content, Iscompleted);
+      const list = await updateListUseCase.execute(id);
       return response.json(list);
     }
 } 
--- BackEnd/src/routes.ts
+++ BackEnd/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateListController } from "./modules/list/useCases/createList/CreateListController";
 import { GetListController } from "./modules/list/useCases/getList/GetListController";
 import { DeleteListController } from "./modules/list/useCases/deleteList/DeleteListController";
+import { UpdateListController } from "./modules/list/useCases/updateList/UpdateListController";
 
 
 const routes = Router();
@@ -9,9 +10,11 @@ const routes = Router();
 const createListController = new CreateListController();
 const getListController = new GetListController();
 const deleteListController = new DeleteListController();
+const updateListController = new UpdateListController();
 
 routes.post("/list", createListController.handle);
 routes.get("/list", getListController.handle);
 routes.delete("/list/:id", deleteListController.handle);
+routes.put("/list/:id", updateListController.handle);
 
 export { routes };
